f9d905c0f33968cc1cab807c2ee244634844e996
Marcos Piske Sendecki <m.piske@hotmail.com>
2016-07-17 19:04:02 -0300

    Configurado a função de cadastrar usuario

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -1,6 +1,24 @@
 <?php 
 
 class CadastroController extends \HXPHP\System\Controller{
+	public function cadastrarAction(){
+		$this->view->setFile('index');
+		//$username = $this->request->post('username');
+		//echo $username;
+		//var_dump($this->request->post());
+		$this->request->setCustomFilters(array(
+			'email' => FILTER_VALIDATE_EMAIL
+			)
+		);
+		$cadastrarUsuario = User::cadastrar($this->request->post());
 
+		var_dump($this->request->post());
+
+
+		//Gerar senha
+		
+		//Obter role_id
+		
+	}
 
 }
